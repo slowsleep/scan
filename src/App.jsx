03f7d825de2596ea4ebc82e3b0c978d0fe2c9656
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-03-18 18:12:07 +0300

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./assets/logo.svg";
+
+function App() {
+
+    return (
+        <>
+            <img src={logo} alt="Scan loog" />
+            <p>hello</p>
+        </>
+    );
+}
+
+export default App;
