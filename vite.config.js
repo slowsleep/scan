d7f86f2f1889da5083e793199903f547a0f08f5a
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-03-31 23:10:07 +0300

    Add aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path, { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve:{
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+      "@components": `${path.resolve(__dirname, "/src/components")}`,
+      "@api": `${path.resolve(__dirname, "/src/api")}`,
+      "@type": `${path.resolve(__dirname, "/src/type")}`,
+      "@assets": `${path.resolve(__dirname, "/src/assets")}`,
+    },
+  },
+});
